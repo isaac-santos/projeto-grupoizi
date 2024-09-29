1c475c8331d439cb63afec8ac279ef6da8a30ab6
isaac-santos <isaacconceicao26@gmail.com>
2024-09-29 16:32:48 -0300

    atualizei o projeto grupoizi

---
+++ src/JavaScript/slide.js
@@ -0,0 +1,43 @@
+const swiper = new Swiper('.swiper', {
+    hashNavigation: true,
+    loop: false,
+  
+    // Paginação
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Setas de navegação
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 1.1,
+      spaceBetween: 40
+    },
+
+    // maior que >= 1440px
+    1440: {
+        slidesPerView: 2,
+        spaceBetween: 15
+    }
+  }
+  });

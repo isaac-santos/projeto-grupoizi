80d29d3d656f5e4e251756a778f5e2d259ddc944
isaac-santos <isaacconceicao26@gmail.com>
2024-09-29 22:18:20 -0300

    atualizei o projeto grupoizi

--- src/JavaScript/slide.js
+++ src/JavaScript/slide.js
@@ -20,8 +20,13 @@ const swiper = new Swiper('.swiper', {
   breakpoints: {
     // when window width is >= 320px
     320: {
-      slidesPerView: 2,
-      spaceBetween: 20
+      slidesPerView: 1,
+      spaceBetween: 200
+    },
+
+    375: {
+      slidesPerView: 1.2,
+      spaceBetween: 30
     },
     // when window width is >= 480px
     480: {

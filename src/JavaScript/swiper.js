b58b048e927b6242fa0bfd62e065565c3ccbc184
isaac-santos <isaacconceicao26@gmail.com>
2024-10-04 21:35:13 -0300

    atualizei o projeto grupoizi e o swiper.js

--- src/JavaScript/swiper.js
+++ src/JavaScript/swiper.js
@@ -1,68 +1,68 @@
 const swiper1 = new Swiper('#swiper1', {
-    effect: 'coverflow',
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  coverflowEffect: {
-    rotate: 50, 
-    stretch: 0, 
-    depth: 100, 
-    modifier: 1, 
-    slideShadows: true, 
-  },
-    loop: false,
-  pagination: {
-    el: '#swiper1 .swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '#swiper1 .swiper-button-next',
-    prevEl: '#swiper1 .swiper-button-prev',
-  },
-  });
-  
-  const swiper2 = new Swiper('#swiper2', {
-    effect: 'coverflow',
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  coverflowEffect: {
-    rotate: 50, 
-    stretch: 0, 
-    depth: 100, 
-    modifier: 1, 
-    slideShadows: true, 
-  },
-    loop: false,
-  pagination: {
-    el: '#swiper2 .swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '#swiper2 .swiper-button-next',
-    prevEl: '#swiper2 .swiper-button-prev',
-  },
-  });
+  effect: 'coverflow',
+grabCursor: true,
+centeredSlides: true,
+slidesPerView: 'auto',
+coverflowEffect: {
+  rotate: 50, 
+  stretch: 0, 
+  depth: 100, 
+  modifier: 1, 
+  slideShadows: true, 
+},
+  loop: false,
+pagination: {
+  el: '#swiper1 .swiper-pagination',
+  clickable: true,
+},
+navigation: {
+  nextEl: '#swiper1 .swiper-button-next',
+  prevEl: '#swiper1 .swiper-button-prev',
+},
+});
 
-  const swiper3 = new Swiper('#swiper3', {
-    effect: 'coverflow',
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  coverflowEffect: {
-    rotate: 50, 
-    stretch: 0, 
-    depth: 100, 
-    modifier: 1, 
-    slideShadows: true, 
-  },
-    loop: false,
-  pagination: {
-    el: '#swiper3 .swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '#swiper3 .swiper-button-next',
-    prevEl: '#swiper3 .swiper-button-prev',
-  },
-  });
+const swiper2 = new Swiper('#swiper2', {
+  effect: 'coverflow',
+grabCursor: true,
+centeredSlides: true,
+slidesPerView: 'auto',
+coverflowEffect: {
+  rotate: 50, 
+  stretch: 0, 
+  depth: 100, 
+  modifier: 1, 
+  slideShadows: true, 
+},
+  loop: false,
+pagination: {
+  el: '#swiper2 .swiper-pagination',
+  clickable: true,
+},
+navigation: {
+  nextEl: '#swiper2 .swiper-button-next',
+  prevEl: '#swiper2 .swiper-button-prev',
+},
+});
+
+const swiper3 = new Swiper('#swiper3', {
+  effect: 'coverflow',
+grabCursor: true,
+centeredSlides: true,
+slidesPerView: 'auto',
+coverflowEffect: {
+  rotate: 50, 
+  stretch: 0, 
+  depth: 100, 
+  modifier: 1, 
+  slideShadows: true, 
+},
+  loop: false,
+pagination: {
+  el: '#swiper3 .swiper-pagination',
+  clickable: true,
+},
+navigation: {
+  nextEl: '#swiper3 .swiper-button-next',
+  prevEl: '#swiper3 .swiper-button-prev',
+},
+});
